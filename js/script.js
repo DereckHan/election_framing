047162eb2f97fa26447a26ecd1c9e47989221310
Ken <xynoci.k@gmail.com>
2016-03-29 00:26:28 -0400

    Minor change

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 var View = require('./views.js');
 var Model = require('./models.js');
 var $ = require("jquery");
+window.$ = window.jQuery = $;
 var d3 = require('d3');
 require('../css/style.css');
 require('../css/xynoci.css');
 require('../css/barchart_style.css');
-window.$ = window.jQuery = $;
 
-var data = "../data/state.json";
-var conditions = new Model.Conditions();
-var bar = new View.barChartView({
-    model: conditions
+$(document).ready(function() {
+    var data = "../data/state.json";
+    var conditions = new Model.Conditions();
+    var bar = new View.barChartView({
+        model: conditions
+    });
+    bar.listenTo(conditions, "change", bar.render);
+    conditions.set("data", data);
 });
-bar.listenTo(conditions, "change", bar.render);
-conditions.set("data", data);
