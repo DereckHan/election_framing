2f7f21a051b79329174c508c21483844e3614625
Ken <xynoci.k@gmail.com>
2016-04-06 04:06:50 -0400

    New json sample and models.

--- js/models.js
+++ js/models.js
@@ -1,10 +1,18 @@
+/*************** 
+ * Dependencies 
+ ***************/
 var $ = require('jquery');
 var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.$ = $;
+var root_url = "",
+    state_file_path = "../data/state_full.json",
+    party_file_path = "",
+    candidate_file_path = "";
 
-var url_root = '';
-
+/*************** 
+ * Models 
+ ***************/
 var Conditions = Backbone.Model.extend({
     defaults: {
         topic: 0,
@@ -17,16 +25,75 @@ var Conditions = Backbone.Model.extend({
     constructor: function() {
         arguments.parse = true;
         Backbone.Model.apply(this, arguments);
+    }
+});
+
+var Line = Backbone.Model.extend({
+    defaults: {
+        keyword: "",
+        datapoints: [],
     },
+    initialize: function() {
+        // load data from server.
+    },
+    conditionChange: function(conditions) {
+        // call when conditions changed
+        // reload parameters from server.
+    },
+
+    constructor: function() {
+        arguments.parse = true;
+        Backbone.Model.apply(this, arguments);
+    }
 });
 
 var Categories = Backbone.Model.extend({
     initialize: function() {
         $.getJSON();
 
-    },
+    }
 });
 
+var Keywords = Backbone.Model.extend({
+    initialize: function() {
+
+    }
+});
+
+var State = Backbone.Model.extend({
+    url: root_url + state_file_path,
+    initialize: function() {}
+});
+
+var Candidate = Backbone.Model.extend({
+    url: root_url + candidate_file_path,
+    initialize: function() {}
+});
+
+var Party = Backbone.Model.extend({
+    url: root_url + party_file_path,
+    initialize: function() {}
+});
+
+
+/***************
+ * Collections 
+ ***************/
+var LineSet = Backbone.Collection.extend({
+    model: Line
+
+});
+
+/***************
+ * exports
+ ***************/
 module.exports = {
-    Conditions: Conditions
+    Conditions: Conditions,
+    Line: Line,
+    Categories: Categories,
+    Keywords: Keywords,
+    State: State,
+    Candidate: Candidate,
+    Party: Party,
+    LineSet: LineSet
 };
--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+/*************** temporary ***************/
+// view_t -> View
+// model_t -> Model
+var view_t = require('./views.js');
+var model_t = require('./models.js');
 var View = require('./views_yuesongli.js');
-var Model = require('./models.js');
+/*************** temporary ***************/
+
 var $ = require("jquery");
 window.$ = window.jQuery = $;
 var d3 = require('d3');
@@ -7,13 +13,31 @@ require('../css/style.css');
 require('../css/xynoci.css');
 require('../css/barchart_style.css');
 
+
 $(document).ready(function() {
     //var data = "../data/state.json";
-    var conditions = new Model.Conditions();
-    var bar = new View.BarSetView({
-        model: conditions
-    });
-    bar.listenTo(conditions, "change", bar.clear);
-    bar.listenTo(conditions, "change", bar.render);
+    // var conditions = new Model.Conditions();
+    // var bar = new View.BarSetView({
+    //     model: conditions
+    // });
+    // bar.listenTo(conditions, "change", bar.clear);
+    // bar.listenTo(conditions, "change", bar.render);
     //conditions.set("data", data);
+
+
+    var state = new model_t.State();
+    var state_data = state.fetch({
+        success: function(model, response, options) {
+            attributes = model.attributes;
+            console.log(attributes["time"]);
+            console.log(attributes["Alabama"]);
+            console.log(attributes["Alabama"]["eco"]);
+            console.log(attributes["Alabama"]["eco"]["day"]);
+            console.log(attributes["Alabama"]["eco"]["day"]["begin_time"]);
+            var terms = attributes["Alabama"]["eco"]["day"]["term_set"];
+            console.log(terms);
+            var key_set = Object.keys(terms);
+            console.log(key_set);
+        }
+    });
 });
