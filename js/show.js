b237e968591d02d95f253d71c5f31949bfbb8f2c
DereckHan <pitthhy@gmail.com>
2016-04-06 16:48:15 -0400

    updated the options and category

--- js/show.js
+++ js/show.js
@@ -27,13 +27,16 @@ var candidates = [ "Hillary Clinton", "Bernie Sanders", "Ted Cruz",
 		"Joseph R. Biden Jr.", "Elizabeth Warren", "Mitt Romney" ];
 var parties = [ "Democratic", "Republican" ];
 function addOption(data) {
+
 	for (var i = 0; i < data.length; i++) {
-		$("div#option-1 select").append("<option>" + data[i] + "</option>");
-		$("div#option-2 select").append("<option>" + data[i] + "</option>");
+		$("div#option-1 select").append("<option value='"+data[i]+"'>" + data[i] + "</option>");
+		$("div#option-2 select").append("<option value='"+data[i]+"'>" + data[i] + "</option>");
 	}
 	;
 	$("div#option-1 select").val(data[0]);
+	$("div#option-2 option[value='"+data[0]+"']").prop('disabled', true);
 	$("div#option-2 select").val(data[1]);
+	$("div#option-1 option[value='"+data[1]+"']").prop('disabled', true);
 	condition.set("option_1", data[0]);
 	condition.set("option_2", data[1]);
 	$("div#option-1 select").selectpicker('refresh');
@@ -60,21 +63,32 @@ $("div#category select").change(function() {
 	$("div#option-2 select").selectpicker('refresh');
 	switch (category) {
 	case "States":
+		condition.set("category", "state");
 		addOption(states);
 		break;
 	case "Parties":
+		condition.set("category", "party");
 		addOption(parties);
 
 		break;
 	case "Candidates":
+		condition.set("category", "candidate");
 		addOption(candidates);
 		break;
 	}
 	;
 });
 $("div#option-1 select").change(function() {
+	var option1 = $(this).val();
+	$("div#option-2 option").prop('disabled', false);
+	$("div#option-2 option[value='"+option1+"']").prop('disabled', true);
+	$("div#option-2 select").selectpicker('refresh');
 	condition.set("option_1", $(this).val());
 });
 $("div#option-2 select").change(function() {
+	var option2 = $(this).val();
+	$("div#option-1 option").prop('disabled', false);
+	$("div#option-1 option[value='"+option2+"']").prop('disabled', true);
+	$("div#option-1 select").selectpicker('refresh');
 	condition.set("option_2", $(this).val());
 });
